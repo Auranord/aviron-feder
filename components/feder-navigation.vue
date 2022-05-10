32fb3b5fe142e6224b5aadd88df3a6fd4a23c37c
auranord <aura.nord@googlemail.com>
2022-05-10 20:16:45 +0200

    Navbar changes.

--- components/feder-navigation.vue
+++ components/feder-navigation.vue
@@ -4,7 +4,7 @@
 
     <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact >
+        <v-list-item v-for="item in drawerItems" :key="item" :to="item.to" router exact >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -16,13 +16,47 @@
     </v-navigation-drawer>
     
     <v-app-bar :clipped-left="clipped" fixed app >
+
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <nuxt-link to="/" >
+        <v-btn icon >
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+      </nuxt-link>
+
       <v-toolbar-title v-text="title" />
+
       <v-spacer />
+
+      <v-menu v-for="navbarItem in navbarItems" :key="navbarItem.title" offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-bind="attrs" v-on="on" text rounded class="my-2" >
+            {{ navbarItem.title }}
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="dropdownItem in navbarItem.dropdownItems" :key="dropdownItem.title" >
+            <nuxt-link :to="dropdownItem.to" :key="dropdownItem.title">
+              <v-list-item-title>{{ dropdownItem.title }}</v-list-item-title>
+            </nuxt-link>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-spacer />
+
       <v-btn icon >
         <v-icon>mdi-account</v-icon>
       </v-btn>
+
+
+
+
+
+
     </v-app-bar>
+
 
   </div>
 
@@ -35,7 +69,64 @@ export default {
       title: 'Aviron Feder',
       clipped: false,
       drawer: false,
-      items: [
+      navbarItems: [
+        {
+          title: 'Test 1',
+          to: '/test',
+          dropdownItems: [
+            {
+              title: 'Test 4', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 5', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 6', 
+              to: '/test', 
+            }, 
+          ],
+        },
+        {
+          title: 'Test 2',
+          to: '/test',
+          dropdownItems: [
+            {
+              title: 'Test 4', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 5', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 6', 
+              to: '/test', 
+            }, 
+          ],
+        },
+        {
+          title: 'Test 3',
+          to: '/test',
+          dropdownItems: [
+            {
+              title: 'Test 4', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 5', 
+              to: '/test', 
+            },
+            {
+              title: 'Test 6', 
+              to: '/test', 
+            }, 
+          ],
+        },
+      ],
+
+      drawerItems: [
         {
           icon: 'mdi-apps',
           title: 'Welcome',
