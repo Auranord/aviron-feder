84037e18115e766a373590bef32e7560678078e8
Auranord <Auranord@users.noreply.github.com>
2022-05-28 12:27:10 +0000

    deploy: f6b817095f6bca6dfb79a553287b6b1a93ee9f88

---
+++ _nuxt/static/1653740827/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Faviron-feder\u002F_nuxt\u002Fstatic\u002F1653740827",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Faviron-feder\u002F",assetsPath:"\u002Faviron-feder\u002F_nuxt\u002F",cdnURL:a}}}}(null));
