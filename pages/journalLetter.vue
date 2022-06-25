4469307395abd7bef5ed16fe3bab550eac9cc563
Druktus <h.nau@tuta.io>
2022-06-25 16:27:36 +0200

    added pages for banks, wallets, journal-issues, -articles, -letters and events

---
+++ pages/journalLetter.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col cols="8">
+                <v-card>
+                    <v-card-title class="headline"> Journal Letters Page </v-card-title>
+                    <v-simple-table>
+                        <template #default>
+                        <thead>
+                            <tr>
+                            <th class="text-left"> Issue </th>
+                            <th class="text-left"> Article </th>
+                            <th class="text-left"> Author </th>
+                            <th class="text-left"> Submission </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="journalLetter in journalLetters"
+                            :key="journalLetter.id"
+                            >
+                            <td>{{ journalLetter.article.issue.date }}</td>
+                            <td>{{ journalLetter.article.title }}</td>
+                            <td>{{ journalLetter.author.name }}</td>
+                            <td>{{ journalLetter.dateTime }}</td>
+                            </tr>
+                        </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'JournalLettersPage',
+  async fetch({store}) {
+      await store.dispatch('loadJournalLetters')
+  },
+  computed: {
+      ...mapState(['journalLetters'])
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/wallets.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col cols="8">
+                <v-card>
+                    <v-card-title class="headline"> Wallets Page </v-card-title>
+                    <v-simple-table>
+                        <template #default>
+                        <thead>
+                            <tr>
+                            <th class="text-left"> Owner </th>
+                            <th class="text-left"> Currency </th>
+                            <th class="text-left"> Balance </th>
+                            <th class="text-left"> Maximum </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="wallet in wallets"
+                            :key="wallet.id"
+                            >
+                            <td>{{ wallet.figure.name }}</td>
+                            <td>{{ wallet.currency.currencyName }}</td>
+                            <td>{{ wallet.balance }}</td>
+                            <td>{{ wallet.maximum }}</td>
+                            </tr>
+                        </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'WalletsPage',
+  async fetch({store}) {
+      await store.dispatch('loadWallets')
+  },
+  computed: {
+      ...mapState(['wallets'])
+  },
+}
+</script>
+
+<style>
+
+</style>
