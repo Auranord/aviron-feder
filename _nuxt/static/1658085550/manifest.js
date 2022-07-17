ab8a0716e59201e1e4648723ab05024059d0c5d5
Druktus <Druktus@users.noreply.github.com>
2022-07-17 19:19:15 +0000

    deploy: ccf695c44796f2c66e108491ecae86a0e8fc666b

---
+++ _nuxt/static/1658085550/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FjournalArticles","\u002Ffactions","\u002FjournalIssues","\u002Fbanks","\u002Flogin","\u002FjournalLetter","\u002Fwallets","\u002Fregister"]})
