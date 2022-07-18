f481452df80dc137134c6c534566b880595b7a59
auranord <aura.nord@googlemail.com>
2022-07-18 06:49:06 +0200

    Added journals test page.

---
+++ pages/journals.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col cols="8">
+                <v-card v-for="journal in journals" :key="journal.id">
+                    <v-card-title class="headline">Ausgabe: {{ journal.date }}</v-card-title>
+                    <v-simple-table v-for="article in journal.articles" :key="article.title">
+                        <template #default>
+                        <thead>
+                            <tr>
+                                <th class="text-left">Title</th>
+                                <th class="text-left">Subtitle</th>
+                                <th class="text-left">Author</th>
+                                <th class="text-left">Category</th>
+                                <th class="text-left">Letters</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{{ article.title }}</td>
+                                <td>{{ article.subtitle }}</td>
+                                <td>{{ article.author.name }}</td>
+                                <td>{{ article.category }}</td>
+                                <td>{{ article.articleLetters }}</td>
+                            </tr>
+                        </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'JournalPage',
+  async fetch({store}) {
+      await store.dispatch('loadJournals')
+  },
+  computed: {
+      ...mapState(['journals'])
+  },
+}
+</script>
+
+<style>
+
+</style>
--- store/index.js
+++ store/index.js
@@ -1,7 +1,15 @@
 export const state = () => ({
     healthcheck: true,
     users: [],
+    user: {},
     figures: [],
+    figure: {},
+    banks: [],
+    bank: {},
+    wallets: [],
+    wallet: {},
+    journals: [],
+    journal: {},
 })
   
 export const mutations = {
@@ -11,8 +19,32 @@ export const mutations = {
     SET_USERS (state, users) {
         state.users = users
     },
+    SET_USER (state, user) {
+        state.user = user
+    },
     SET_FIGURES (state, figures) {
         state.figures = figures
+    },
+    SET_FIGURE (state, figure) {
+        state.figure = figure
+    },
+    SET_BANKS (state, banks) {
+        state.banks = banks
+    },
+    SET_BANK (state, bank) {
+        state.bank = bank
+    },
+    SET_WALLETS (state, wallets) {
+        state.wallets = wallets
+    },
+    SET_WALLET (state, wallet) {
+        state.wallet = wallet
+    },
+    SET_JOURNALS (state, journals) {
+        state.journals = journals
+    },
+    SET_JOURNAL (state, journal) {
+        state.journal = journal
     },
 }
 
@@ -29,9 +61,41 @@ export const actions = {
         const response = await this.$axios.get('/abakus/user/all')
         commit('SET_USERS', response.data)
     },
+    async loadUser({commit}) {
+        const response = await this.$axios.get('/abakus/user/')
+        commit('SET_USER', response.data)
+    },
     async loadFigures({commit}) {
         const response = await this.$axios.get('/abakus/figure/all')
         commit('SET_FIGURES', response.data)
+    },
+    async loadFigure({commit}) {
+        const response = await this.$axios.get('/abakus/figure/')
+        commit('SET_FIGURE', response.data)
+    },
+    async loadBanks({commit}) {
+        const response = await this.$axios.get('/abakus/bank/all')
+        commit('SET_BANKS', response.data)
+    },
+    async loadBank({commit}) {
+        const response = await this.$axios.get('/abakus/bank/')
+        commit('SET_BANK', response.data)
+    },
+    async loadWallets({commit}) {
+        const response = await this.$axios.get('/abakus/bank_wallet/all')
+        commit('SET_WALLETS', response.data)
+    },
+    async loadWallet({commit}) {
+        const response = await this.$axios.get('/abakus/bank_wallet/')
+        commit('SET_WALLET', response.data)
+    },
+    async loadJournals({commit}) {
+        const response = await this.$axios.get('/abakus/journal/all')
+        commit('SET_JOURNALS', response.data)
+    },
+    async loadJournal({commit}, id) {
+        const response = await this.$axios.get('/abakus/journal/' + id)
+        commit('SET_JOURNAL', response.data)
     },
 }
 
