0092943bc692f849c354c9b2419aa0aee3fd664f
Druktus <102549895+Druktus@users.noreply.github.com>
2022-07-18 06:45:31 +0200

    Added-Landing-Page (#3)
    * Files for landing page with dummy screenshot.

---
+++ layouts/landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app light>
+    <feder-landing-toolbar />
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <feder-footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'LandingLayout'
+}
+</script>
